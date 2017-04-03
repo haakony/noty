b3cb0ab8b3737edad8d91e13854aaf98c82cbd91
haakony <haakony@gmail.com>
2017-04-03 16:28:23 +0200

    hackerwars last tick to luis

--- app.js
+++ app.js
@@ -67,6 +67,40 @@ intents.matches('GetTempFromApi', [
 ]);
 
 
+
+intents.matches('GetLastTickHackerWars', [
+    function (session) {
+    var http = require("http");
+
+    var options = {
+        host: 'hacker-wars.com',
+        path: '/lasttick'
+    };
+
+    http.get(options, function (http_res) {
+        // initialize the container for our data
+        var data = "";
+
+        // this event fires many times, each time collecting another piece of the response
+        http_res.on("data", function (chunk) {
+            // append this chunk to our growing `data` var
+            data += chunk;
+        });
+
+        // this event fires *one* time, after all the `data` events/chunks have been gathered
+        http_res.on("end", function () {
+            // you can use res.send instead of console.log to output via express
+            session.send("The last tick was %s seconds ago", data);
+        });
+    });
+
+}]);
+
+
+
+
+
+
 intents.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
 
 
