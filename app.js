d1373304f1f56b87885538ab6ace2fad3d1073ce
haakony <haakony@gmail.com>
2017-04-03 22:05:03 +0200

    added dicer game, for random simple things

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var zummerStrings = require('./zummer-strings.js');
 var bingSearchService = require('./bing-search-service.js');
 var bingSummarizerService = require('./bing-summarizer-service.js');
 var urlObj = require('url');
-
+//require('./dicer.js');
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -140,6 +140,149 @@ function prepareZummerResult(query, bingSearchResult) {
 }
 
 //end wiki
+
+
+
+
+//dicer
+intents.matches('StartDicer', [
+    function (session) {
+        session.send("Let me pick something 'random' for you.");
+        session.beginDialog('rootMenu');
+    },
+    function (session, results) {
+        session.endConversation("Whatever, just dont leave me.");
+    }
+]);
+
+// Add root menu dialog
+bot.dialog('rootMenu', [
+    function (session) {
+        builder.Prompts.choice(session, "Choose an option:", 'Flip A Coin|Roll Dice|Magic 8-Ball|Quit');
+    },
+    function (session, results) {
+        switch (results.response.index) {
+            case 0:
+                session.beginDialog('flipCoinDialog');
+                break;
+            case 1:
+                session.beginDialog('rollDiceDialog');
+                break;
+            case 2:
+                session.beginDialog('magicBallDialog');
+                break;
+            default:
+                session.endDialog();
+                break;
+        }
+    },
+    function (session) {
+        // Reload menu
+        session.replaceDialog('rootMenu');
+    }
+]).reloadAction('showMenu', null, { matches: /^(menu|back)/i });
+
+// Flip a coin
+
+intents.matches('flipCoinDialog', [
+    function (session) {
+        session.beginDialog('flipCoinDialog');
+    },
+]);
+
+
+bot.dialog('flipCoinDialog', [
+    function (session, args) {
+        builder.Prompts.choice(session, "Choose heads or tails.", "heads|tails", { listStyle: builder.ListStyle.none })
+    },
+    function (session, results) {
+        var flip = Math.random() > 0.5 ? 'heads' : 'tails';
+        if (flip == results.response.entity) {
+            session.endDialog("It's %s. I bet you cheeted!", flip);
+        } else {
+            session.endDialog("I WIN! It was %s. you lost you looser! :(", flip);
+        }
+    }
+]);
+
+// Roll some dice
+intents.matches('rollDiceDialog', [
+    function (session) {
+        session.beginDialog('rollDiceDialog');
+    },
+]);
+
+bot.dialog('rollDiceDialog', [
+    function (session, args) {
+        builder.Prompts.number(session, "How many dice should I roll?");
+    },
+    function (session, results) {
+        if (results.response > 0) {
+            var msg = "I rolled:";
+            for (var i = 0; i < results.response; i++) {
+                var roll = Math.floor(Math.random() * 6) + 1;
+                msg += ' ' + roll.toString();
+            }
+            session.endDialog(msg);
+        } else {
+            session.endDialog("Ummm... Ok... I rolled air with my nonexisting hands.");
+        }
+    }
+]);
+
+// Magic 8-Ball
+intents.matches('magicBallDialog', [
+    function (session) {
+        session.beginDialog('magicBallDialog');
+    },
+]);
+
+bot.dialog('magicBallDialog', [
+    function (session, args) {
+        builder.Prompts.text(session, "What is your question?");
+    },
+    function (session, results) {
+        // Use the SDK's built-in ability to pick a response at random.
+        session.endDialog(magicAnswers);
+    }
+]);
+
+var magicAnswers = [
+    "It is certain",
+    "Who knows ?",
+    "You are wierd",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+];
+
+//end dicer
+
+
+
+
+
+
+
+
+
+
 
 
 intents.matches('SayTest', [
--- zummer-strings.js
+++ zummer-strings.js
@@ -4,7 +4,7 @@
     GreetOnDemand: "Hi! What would you like to read about? I can fetch you the best Wikipedia results on any subject.",
     MoreOnBing: "More on Bing",
     PoweredBy: "Powered by %s",
-    SearchTopicTypeMessage: "Wow you realy are lazy arent you ?, well this is what i found.",
+    SearchTopicTypeMessage: "You realy are lazy arent you ? Well this is what i found.",
     SummaryErrorMessage: "Hmm, I'm sorry. I could not summarize the article.",
     SummaryString: "SUMMARY"
 }
